b457cf914ba575639eb6d50f290fdaf5209c2c6b
who am i <62942231+sachira-madhushan@users.noreply.github.com>
2023-07-30 08:55:45 +0530

    Initial commit

---
+++ Assets/Scripts/VirtualButton.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+public class VirtualButton : MonoBehaviour
+{
+    public void ButtonClick()
+    {
+        
+        Application.OpenURL("http://e404developers.com");
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# E404-Developers-Business-Card
+ Unity AR business card  project
